d1821ab8966c5374ebafd5def03d7115442364d4
Ruslan Lutfullin <jeudesprits@icloud.com>
2020-01-01 14:37:43 +0500

    Initial commit

--- Sources/PSLock/PSUnfairLock.swift
+++ Sources/PSLock/PSUnfairLock.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 @available(OSX 10.12, *)
-final class PSUnfairLock {
+public final class PSUnfairLock {
   
   private var _lock = os_unfair_lock()
   
-  func lock() { os_unfair_lock_lock(&_lock) }
+  public func lock() { os_unfair_lock_lock(&_lock) }
   
-  func unlock() { os_unfair_lock_unlock(&_lock) }
+  public func unlock() { os_unfair_lock_unlock(&_lock) }
 }
