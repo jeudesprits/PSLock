9d5273d72050fc7c9ae86f2a74a4ba61b9a9f103
Ruslan Lutfullin <jeudesprits@icloud.com>
2020-01-01 16:43:08 +0500

    New version 1.1.0

--- Package.swift
+++ Package.swift
@@ -17,8 +17,5 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(name: "PSLock", dependencies: []),
     .testTarget(name: "PSLockTests", dependencies: ["PSLock"]),
-  ],
-  swiftLanguageVersions: [
-    .v5,
   ]
 )
