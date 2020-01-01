a16d85dd570bfa3565c440e87224aac6d6f98f07
Ruslan Lutfullin <jeudesprits@icloud.com>
2020-01-01 23:07:56 +0500

    Fixed target iOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "PSLock",
   platforms: [
-    .iOS("13.0"),
+    .iOS(.v13),
   ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
